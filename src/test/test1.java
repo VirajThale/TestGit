c4bffb9f7a2dd76c1cbae642f04aeda79e98bad8
Viraj Thale (vthale) <vthale@cisco.com>
2021-09-27 22:48:53 -0700

    27Sept demo

---
+++ src/test/test1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Test1");
+			System.out.println("Your Test2");
+			
+	}
+
+}
